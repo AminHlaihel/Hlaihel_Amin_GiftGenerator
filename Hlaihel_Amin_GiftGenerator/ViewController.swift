b47e79aec3908c322eff2ea9ecd3d447c49a564e
Amin Hlaihel <aminhlaihel@Amins-iMac.local>
2018-12-28 20:11:36 +0100

    Initial Commit

---
+++ Hlaihel_Amin_GiftGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hlaihel_Amin_GiftGenerator
+//
+//  Created by Amin Hlaihel on 28/12/2018.
+//  Copyright © 2018 IF26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
